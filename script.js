d0eabbde6756180dc4dbbb9b154914c3f5f15556
ravikant8789 <devikalawati425@gmail.com>
2026-01-02 23:40:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ function toggleDarkMode() {
   document.body.classList.toggle("dark");
 }
 
+window.onload = function () {
+  if (!localStorage.getItem("videoSeen")) {
+    document.getElementById("videoPopup").style.display = "flex";
+    localStorage.setItem("videoSeen", "true");
+  }
+};
+
+function closePopup() {
+  document.getElementById("videoPopup").style.display = "none";
+}
+
+const text = "CSE Student | Cybersecurity | AI | Problem Solver";
+let i = 0;
+
+function typeEffect() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typeEffect, 80);
+  }
+}
+
+typeEffect();
+
+
